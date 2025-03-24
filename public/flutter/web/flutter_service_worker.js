761d4027526a6e24c18552f0b35b2c8ade56e056
Jagan balasubramanian <balujgn@gmail.com>
2025-03-24 17:52:53 +0530

    changed build file with some bug fixes

--- public/flutter/web/flutter_service_worker.js
+++ public/flutter/web/flutter_service_worker.js
@@ -30,9 +30,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "2f3ec1ed6a9e3112c7ca6b87f5176f5d",
-"/": "2f3ec1ed6a9e3112c7ca6b87f5176f5d",
-"main.dart.js": "287d6114011d50596eaac0a70de3ec49",
+"index.html": "7b93d7472da3f137bb3d6e960ced7c28",
+"/": "7b93d7472da3f137bb3d6e960ced7c28",
+"main.dart.js": "14048ab01afc52270df96a514386f3e1",
 "main.js": "d9f20be58e1b458eae3bf337e0fee747",
 "manifest.json": "8ac32b578c8646b6dd65ea90b98d3e3f",
 "version.json": "534378db55979885de1a4bb33de52d8b"};
