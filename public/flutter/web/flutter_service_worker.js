8351db5f833c7035994764453b70fb4dfdf146f6
Jagan balasubramanian <balujgn@gmail.com>
2025-04-07 13:45:15 +0530

    cvhanged build folder to recent changes

--- public/flutter/web/flutter_service_worker.js
+++ public/flutter/web/flutter_service_worker.js
@@ -30,9 +30,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "6a47502fed8e6d2d3fb09047d53b7530",
-"/": "6a47502fed8e6d2d3fb09047d53b7530",
-"main.dart.js": "7048a62a0ab25254d58efa4c7e1c75a9",
+"index.html": "afc1398232e1155cd3202bf21ffd89c7",
+"/": "afc1398232e1155cd3202bf21ffd89c7",
+"main.dart.js": "c680b84acb1ffc407a8ab2f7eb546856",
 "main.js": "d9f20be58e1b458eae3bf337e0fee747",
 "manifest.json": "8ac32b578c8646b6dd65ea90b98d3e3f",
 "version.json": "534378db55979885de1a4bb33de52d8b"};
